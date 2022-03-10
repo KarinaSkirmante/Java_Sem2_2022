4a8a657725ae1783f97091f9fa3f705d908bb233
Karina Skirmante <karina.krinkele@venta.lv>
2022-03-10 11:28:57 +0200

    Add: new class Student with variables

---
+++ src/models/Student.java
@@ -0,0 +1,14 @@
+package models;
+
+public class Student {
+	//1.mainīgie
+	private int id;
+	private String name;
+	private String surname;
+	
+	private static int idCounter = 10000;
+	//2.1. get funkcijas
+	//2.2. set funkcijas
+	//3.konstruktori
+	//4.toString funkcija
+}

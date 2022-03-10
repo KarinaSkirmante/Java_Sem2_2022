3c3b9ef0acf711563ef17000d38798c29d9f663c
Karina Skirmante <karina.krinkele@venta.lv>
2022-03-10 11:10:41 +0200

    Edit: Course get and sset functions

--- src/models/Course.java
+++ src/models/Course.java
@@ -7,5 +7,75 @@ public class Course {
 	private byte creditPoints;
 	private Professor professor;
 	
+	private static int idCounter = 1000;
+	//2.1. get funkcijas 
+
+	public int getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public byte getCreditPoints() {
+		return creditPoints;
+	}
+
+	public Professor getProfessor() {
+		return professor;
+	}
+
+	//2.2. set funkcijas
+	public void setId() {
+		this.id = idCounter;
+		idCounter++;
+	}
+
+	public void setTitle(String title) {
+		if(title!=null && !title.isEmpty() && title.matches("[A-ZĀĒŪĪĻĶĢŠŽČŅ]{1}[\\s\\d a-zēūīāšģķļņčžA-ZĀĒŪĪĻĶĢŠŽČŅ]+"))
+		{
+			this.title = title;
+		}
+		else
+		{
+			this.title  ="unknown";
+		}
+	}
+
+	public void setCreditPoints(byte creditPoints) {
+		if(creditPoints>0 && creditPoints <=20)
+		{
+			this.creditPoints = creditPoints;
+		}
+		else
+		{
+			this.creditPoints = 2;
+		}
+		
+	}
+
+	
+	public void setProfessor(Professor professor) {
+		if(professor!=null)
+		{
+			this.professor = professor;
+		}
+		else
+		{
+			this.professor = new Professor();
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	//3.konstruktori
+	
+	//4. toString funkcija
 
 }

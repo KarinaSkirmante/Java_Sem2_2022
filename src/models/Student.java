7dfe2ede07277bd8fbc9d6d383d564e54de897fb
Karina Skirmante <karina.krinkele@venta.lv>
2022-03-10 11:34:18 +0200

    Edit: Student get and sset functions

--- src/models/Student.java
+++ src/models/Student.java
@@ -8,7 +8,45 @@ public class Student {
 	
 	private static int idCounter = 10000;
 	//2.1. get funkcijas
+	public int getId() {
+		return id;
+	}	
+	public String getName() {
+		return name;
+	}
+	public String getSurname() {
+		return surname;
+	}
 	//2.2. set funkcijas
+	public void setId()
+	{
+		this.id  =idCounter;
+		idCounter++;
+	}
+	public void setName(String name) { 
+		if(name!=null && name.matches("[A-ZĀĒŪĪĻĶĢŠŽČŅ]{1}[a-zēūīāšģķļņčž]+")) //\p{L}+
+		{
+			this.name = name;
+		}
+		else
+		{
+			this.name = "notknown";
+		}
+	}
+	
+	public void setSurname(String surname)
+	{
+		if(surname!=null && surname.matches("[A-ZĀĒŪĪĻĶĢŠŽČŅ]{1}[a-zēūīāšģķļņčž]+")) //\p{L}+
+		{
+			this.surname = surname;
+		}
+		else
+		{
+			this.surname = "notknown";
+		}
+	}
+	
+	
 	//3.konstruktori
 	//4.toString funkcija
 }

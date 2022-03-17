a58314e592e6ddb34f0a143068e26e23ddc9e170
Karina Skirmante <karina.krinkele@venta.lv>
2022-03-17 11:50:13 +0200

    Edit: read function for Course

--- src/service/VeAForumService.java
+++ src/service/VeAForumService.java
@@ -151,7 +151,26 @@ public class VeAForumService {
 			return true;
 		}
 	}
+	//R - read -all
+	private static ArrayList<Course> getAllCourses()
+	{
+		return allCourses;
+	}
 	
+	//R - read by ID
+	private static Course getCourseById(int courseId)
+	{
+		if(courseId>=1000 && courseId<10000)
+		{
+			for (Course course : allCourses) {
+				if(course.getId()==courseId)
+					{
+						return course;
+					}
+				}
+			}
+		return new Course();
+	}
 	
 
 }
